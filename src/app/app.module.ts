b432cafecc8567e50f007cd58c9920f823fccd39
Johan Garcia <johan123garciaherran@gmail.com>
2020-03-23 02:53:37 -0500

    Plantilla Base Front Control de acceso unipiloto SAM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from "@angular/forms";
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from "./directives/resaltar.directive";
+import { ContarClicksDirective } from "./directives/contar-clicks.directive";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from "@angular/router";
+import { DetalleComponent } from "./detalle/detalle.component";
+import { LugaresComponent } from "./lugares/lugares.component";
+import { CrearComponent } from "./crear/crear.component";
+const appRoutes: Routes = [
+  { path: '', component: LugaresComponent },
+  { path: 'lugares', component: LugaresComponent },
+  { path: 'detalle/:id', component: DetalleComponent },
+  { path: 'crear', component: CrearComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective,
+    DetalleComponent,
+    LugaresComponent,
+    CrearComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCiGsoFevMN2J-dXWtD_31AN4UkraR4Hq0'
+    }),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/crear/crear.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-crear',
+    templateUrl: './crear.component.html'
+})
+export class CrearComponent {
+    lugar: any = {};
+    constructor() {
+
+    }
+    guardarLugar() {
+    }
+}
